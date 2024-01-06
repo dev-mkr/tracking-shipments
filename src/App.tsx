ac9643f285b620640cc484d3d6fc23e1ef7c99f1
dev-mkr <webdev.mu@gamil.com>
2024-01-06 05:45:23 +0200

    init: i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Suspense, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 function App() {
-  return <></>;
+  const { t, i18n } = useTranslation();
+  const { language, changeLanguage } = i18n;
+  const handleChangeLanguage = () => {
+    const newLanguage = language === "en" ? "ar" : "en";
+    changeLanguage(newLanguage);
+  };
+  useEffect(() => {
+    document.documentElement.lang = language;
+    document.documentElement.dir = language === "ar" ? "rtl" : "ltr";
+  }, [language]);
+
+  return (
+    <Suspense fallback={<div>loading</div>}>
+      <button onClick={handleChangeLanguage}>change</button>
+      <div>{t("description.part1")}</div>
+    </Suspense>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@/styles/index.css";
 import "virtual:svg-icons-register";
+import "@/libs/i18n.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
