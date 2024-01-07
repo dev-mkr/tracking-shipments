f762fd5574e15d01121ae3d5000ac0fc4afa997d
dev-mkr <webdev.mu@gamil.com>
2024-01-07 10:23:49 +0200

    make: arabic default language

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -12,7 +12,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en",
+    fallbackLng: "ar",
     backend: {
       loadPath: "/locales/{{lng}}/translation.json",
     },
