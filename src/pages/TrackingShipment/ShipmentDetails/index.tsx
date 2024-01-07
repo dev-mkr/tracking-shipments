f12b322dd1e8099aa5900a399c65bdbad7c2d64a
dev-mkr <webdev.mu@gamil.com>
2024-01-07 10:09:34 +0200

    created: shipment details

---
+++ src/pages/TrackingShipment/ShipmentDetails/index.tsx
@@ -0,0 +1,83 @@
+import { ShipmentStatues } from "@/shared/types/shipment.types";
+import { cn } from "@/shared/utils/cn";
+import { useTranslation } from "react-i18next";
+
+interface Props {
+  shipmentNumber: string;
+  currentStatus: string;
+  lastUpdate: string;
+  provider: string;
+  promisedDeliveryDate: string;
+}
+
+export const ShipmentDetails = ({
+  shipmentNumber,
+  currentStatus,
+  lastUpdate,
+  provider,
+  promisedDeliveryDate,
+}: Partial<Props>) => {
+  const { t, i18n } = useTranslation();
+
+  const renderLastUpdate = (time?: string, lang?: string) => {
+    if (!time) return "";
+    const date = new Date(time);
+
+    return `${date.toLocaleString(lang, {
+      weekday: "long",
+    })} ${date.toLocaleString("en", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    })}`;
+  };
+
+  const renderPromisedDeliveryDate = (time?: string, lang?: string) => {
+    if (!time) return "";
+    return new Date(time).toLocaleString(lang, {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
+  return (
+    <div className="flex flex-wrap justify-between gap-5 px-5">
+      <div>
+        <span className="text-gray-400">
+          {t("shipment.tracking-number")} {shipmentNumber}
+        </span>
+        <p
+          className={cn("mt-2 text-lg font-extrabold text-primary", {
+            "text-green-400": currentStatus === ShipmentStatues.DELIVERED,
+            "text-main": currentStatus === ShipmentStatues.CANCELLED,
+            "text-yellow-300":
+              currentStatus === ShipmentStatues.WAITING_FOR_CUSTOMER_ACTION,
+          })}
+        >
+          {t(`shipment.status.${currentStatus}`, "")}
+        </p>
+      </div>
+      <div>
+        <span className="text-gray-400">{t("shipment.last-update")} </span>
+        <p className="mt-2 text-lg font-extrabold text-primary">
+          {renderLastUpdate(lastUpdate, i18n.language)}
+        </p>
+      </div>
+      <div>
+        <span className="text-gray-400">{t("shipment.provider-name")} </span>
+        <p className="mt-2 text-lg font-extrabold text-primary">{provider}</p>
+      </div>
+      <div>
+        <span className="text-gray-400">
+          {t("shipment.promised-delivery-date")}{" "}
+        </span>
+        <p className="mt-2 text-lg font-extrabold text-primary">
+          {renderPromisedDeliveryDate(promisedDeliveryDate, i18n.language)}
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/shared/TrackingNumberInput.tsx
+++ src/shared/TrackingNumberInput.tsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useShipmentStore } from "@/store/shipment-store";
 import SvgIcon from "./SvgIcon";
 
-export const TrackingNumberInput = () => {
+interface Props {
+  callBack?: () => void;
+}
+
+export const TrackingNumberInput = ({ callBack }: Props) => {
   const { t } = useTranslation();
+
+  const [shipmentNumberInput, setShipmentNumberInput] = useState("");
+
+  const setShipmentNumber = useShipmentStore(
+    (state) => state.setShipmentNumber
+  );
+
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3" onClick={(e) => e.preventDefault()}>
       <label className="text-start">{t("tracking-input.label")}</label>
 
       <div className="relative ">
         <input
           type="search"
-          id="search-dropdown"
+          id="search-input"
+          value={shipmentNumberInput}
+          onChange={(e) => setShipmentNumberInput(e.target.value)}
           autoFocus
           className="h-12 w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-main focus:outline-none focus:ring-main "
           placeholder={t("tracking-input.placeholder")}
         />
-        <button className="absolute end-0 top-0 h-full rounded-e-lg border bg-main p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-main ">
+        <button
+          onClick={() => {
+            setShipmentNumber(shipmentNumberInput);
+            callBack?.();
+          }}
+          className="absolute end-0 top-0 h-full rounded-e-lg border bg-main p-2.5 text-sm font-medium text-white hover:bg-main focus:outline-none focus:ring-4 focus:ring-main "
+        >
           <SvgIcon className="h-6 w-6" name="search-icon" alt="search" />
         </button>
       </div>
